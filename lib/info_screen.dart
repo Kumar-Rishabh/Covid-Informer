fa01c389dadd5bb8dfad42b667fcb9ec9d2dbc56
DIP-POLLEY <dipanpolley2000@gmail.com>
2022-05-30 23:44:20 +0530

    updating country list

--- lib/info_screen.dart
+++ lib/info_screen.dart
@@ -7,66 +7,68 @@ import 'package:flutter_svg/flutter_svg.dart';
 class Info_Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            MyHeader(
-                image: "assets/icons/coronadr.svg", 
-                textTop: "Get to know", 
-                textBottom: "About covid-19."
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                      "Symptoms",
-                    style: kTitleTextstyle,
-                  ),
-                  SizedBox(height: 20),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      SymptomCard(
-                        image: "assets/images/headache.png",
-                        title: "Headache",
-                        isActive: true,
-                      ),
-                      SymptomCard(
-                        image: "assets/images/caugh.png",
-                        title: "Caugh",
-                      ),
-                      SymptomCard(
-                        image: "assets/images/fever.png",
-                        title: "Fever",
-                      ),
-
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Text("Prevention", style: kTitleTextstyle),
-                  SizedBox(height: 20),
-                  PreventCard(
-                    text:
-                    "Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks",
-                    image: "assets/images/wear_mask.png",
-                    title: "Wear face mask",
-                  ),
-                  PreventCard(
-                    text:
-                    "To prevent the spread of germs during the COVID-19 pandemic, "
-                        "you should also wash your hands with soap and water for at least 20 seconds",
-                    image: "assets/images/wash_hands.png",
-                    title: "Wash your hands",
-                  ),
-                  SizedBox(height: 50),
-                ],
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              MyHeader(
+                  image: "assets/icons/coronadr.svg", 
+                  textTop: "Get to know", 
+                  textBottom: "About covid-19."
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                        "Symptoms",
+                      style: kTitleTextstyle,
+                    ),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        SymptomCard(
+                          image: "assets/images/headache.png",
+                          title: "Headache",
+                          isActive: true,
+                        ),
+                        SymptomCard(
+                          image: "assets/images/caugh.png",
+                          title: "Caugh",
+                        ),
+                        SymptomCard(
+                          image: "assets/images/fever.png",
+                          title: "Fever",
+                        ),
+
+                      ],
+                    ),
+                    SizedBox(height: 20),
+                    Text("Prevention", style: kTitleTextstyle),
+                    SizedBox(height: 20),
+                    PreventCard(
+                      text:
+                      "Since the start of the coronavirus outbreak some places have fully embraced wearing facemasks",
+                      image: "assets/images/wear_mask.png",
+                      title: "Wear face mask",
+                    ),
+                    PreventCard(
+                      text:
+                      "To prevent the spread of germs during the COVID-19 pandemic, "
+                          "you should also wash your hands with soap and water for at least 20 seconds",
+                      image: "assets/images/wash_hands.png",
+                      title: "Wash your hands",
+                    ),
+                    SizedBox(height: 50),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:covid_19/components/network.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -28,174 +29,218 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+
+
+  int lst = 10;
+
+  String country="Select";
+  List<String> countrylist=[
+    'Select',
+
+  ];
+
+  void getcountries() async
+  {
+    NetworkHelper networkhelper=NetworkHelper("https://api.covid19api.com/summary");
+    var data=await networkhelper.getData();
+    lst = data["Countries"].length;
+
+    for(int i=0;i<lst;i++)
+      {
+
+
+        setState(() {
+          countrylist.add(data["Countries"][i]["Country"]);
+        });
+      }
+    print(data["Countries"].length);
+    //return weatherData;
+  }
+
+  @override
+  void initState() {
+    getcountries();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: <Widget>[
 
-          MyHeader(
-            image: "assets/icons/Drcorona.svg",
-            textTop: "All you need",
-            textBottom: "is stay at home.",
-            //offset: offset,
-          ),
+            MyHeader(
+              image: "assets/icons/Drcorona.svg",
+              textTop: "All you need",
+              textBottom: "is stay at home.",
+              //offset: offset,
+            ),
 
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20),
-            padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-            height: 60,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(25),
-              border: Border.all(
-                color: Color(0XFFE5E5E5),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              padding: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+              height: 60,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+                border: Border.all(
+                  color: Color(0XFFE5E5E5),
+                ),
+              ),
+              child: Row(
+                children: <Widget>[
+                  SvgPicture.asset("assets/icons/maps-and-flags.svg"),
+                  SizedBox(width: 20),
+                  Expanded(
+                      child: DropdownButton(
+                        isExpanded: true,
+                        underline: SizedBox(),
+                        icon: SvgPicture.asset("assets/icons/dropdown.svg"),
+                        value: country,
+                        items: countrylist.map<DropdownMenuItem<String>>((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (value) {
+                          setState(() {
+                            country = value.toString();
+                            print(country);
+                          });
+                        },
+                      ),
+                  ),
+                ],
               ),
             ),
-            child: Row(
-              children: <Widget>[
-                SvgPicture.asset("assets/icons/maps-and-flags.svg"),
-                SizedBox(width: 20),
-                Expanded(
-                    child: DropdownButton(
-                      isExpanded: true,
-                      underline: SizedBox(),
-                      icon: SvgPicture.asset("assets/icons/dropdown.svg"),
-                      value: "Indonesia",
-                      items: [
-                        'Indonesia',
-                        'Bangladesh',
-                        'United States',
-                        'Japan'
-                      ].map<DropdownMenuItem<String>>((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (value) {},
+            SizedBox(height: 20),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: <Widget>[
+                Row(
+                    children: <Widget>[
+                      RichText(
+                          text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "Case Update\n",
+                              style: kTitleTextstyle,
+                            ),
+                            TextSpan(
+                              text: "Newest update March 28",
+                              style: TextStyle(
+                              color: kTextLightColor,
+                            ),
+                          ),
+                      ],
                     ),
-                ),
-              ],
-            ),
-          ),
-          SizedBox(height: 20),
-          Padding(
-              padding: EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: <Widget>[
-              Row(
-                  children: <Widget>[
-                    RichText(
-                        text: TextSpan(
-                        children: [
-                          TextSpan(
-                            text: "Case Update\n",
-                            style: kTitleTextstyle,
-                          ),
-                          TextSpan(
-                            text: "Newest update March 28",
-                            style: TextStyle(
-                            color: kTextLightColor,
-                          ),
+                  ),
+                      Spacer(),
+                      Text(
+                        "See details",
+                        style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.w600,
                         ),
-                    ],
-                  ),
-                ),
-                    Spacer(),
-                    Text(
-                      "See details",
-                      style: TextStyle(
-                        color: kPrimaryColor,
-                        fontWeight: FontWeight.w600,
                       ),
+                ],
+              ),
+                  SizedBox(height: 20),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 4),
+                          blurRadius: 30,
+                          color: kShadowColor,
+                        ),
+                      ],
                     ),
-              ],
-            ),
-                SizedBox(height: 20),
-                Container(
-                  padding: EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 4),
-                        blurRadius: 30,
-                        color: kShadowColor,
-                      ),
-                    ],
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Counter(
+                          color: kInfectedColor,
+                          number: 1046,
+                          title: "Infected",
+                        ),
+                        Counter(
+                          color: kDeathColor,
+                          number: 87,
+                          title: "Deaths",
+                        ),
+                        Counter(
+                          color: kRecovercolor,
+                          number: 46,
+                          title: "Recovered",
+                        ),
+                      ],
+                    ),
                   ),
-                  child: Row(
+                  SizedBox(height: 20),
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      Counter(
-                        color: kInfectedColor,
-                        number: 1046,
-                        title: "Infected",
+                      Text(
+                        "Spread of Virus",
+                        style: kTitleTextstyle,
                       ),
-                      Counter(
-                        color: kDeathColor,
-                        number: 87,
-                        title: "Deaths",
-                      ),
-                      Counter(
-                        color: kRecovercolor,
-                        number: 46,
-                        title: "Recovered",
+                      Text(
+                        "See details",
+                        style: TextStyle(
+                          color: kPrimaryColor,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ],
                   ),
-                ),
-                SizedBox(height: 20),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      "Spread of Virus",
-                      style: kTitleTextstyle,
-                    ),
-                    Text(
-                      "See details",
-                      style: TextStyle(
-                        color: kPrimaryColor,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ],
-                ),
-                Container(
+                  Container(
 
-                  margin: EdgeInsets.only(top: 10),
-                  padding: EdgeInsets.all(10),
-                  height: 108,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white,
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 30,
-                        color: kShadowColor,
-                      ),
-                    ],
+                    margin: EdgeInsets.only(top: 10),
+                    padding: EdgeInsets.all(10),
+                    height: 108,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 10),
+                          blurRadius: 30,
+                          color: kShadowColor,
+                        ),
+                      ],
+                    ),
+                    child: Image.asset(
+                      "assets/images/map.png",
+                      fit: BoxFit.contain,
+                    ),
                   ),
-                  child: Image.asset(
-                    "assets/images/map.png",
-                    fit: BoxFit.contain,
-                  ),
-                ),
+            ],
+              ),
+              ),
           ],
-            ),
-            ),
-        ],
+        ),
       ),
     );
   }
 }
+
+
 
 
 
